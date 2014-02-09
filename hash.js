adac5a70dddf22463b9c6e3541edaaec2884f3e6
Aubrey Portwood <aubreypwd@gmail.com>
2014-02-08 23:26:08 -0700

    Don't do some JS stuff when we aren't in a hash

--- hash.js
+++ hash.js
@@ -2,6 +2,43 @@ h_table_interval='';
 h_table_ajax='';
 h_screen_animate='';
 
+$(document).ready(function(){
+
+	if(
+			php_._hash==''
+		|| php_.nick==''
+	) return false;
+
+	h_make_table_interval();
+	goto_screen_bottom();
+
+	if( $('#message') ){
+		$('#message').keypress(function(e) {
+			if(e.which == 13) {
+				$.ajax({
+					url: 'hash.php',
+					method: 'get',
+					data: {
+						action: 'post_message',
+						message: $('#message').val(),
+						nick: php_.nick,
+						hash: php_._hash
+					},
+					success: function(
+						data, 
+						textStatus, 
+						jqXHR
+					){
+						$('#message').val('');
+						$('#messages').html(data);
+						goto_screen_bottom();
+					}
+				});
+			}
+		});
+	}
+});
+
 function h_kill_table_interval(){
 	clearInterval(h_table_interval);
 }
@@ -26,7 +63,7 @@ function h_make_table_interval(){
 					$(window).scrollTop() + $(window).height()
 						>= $(document).height() - 50
 				){
-					h_goto_bottom_of_screen();
+					goto_screen_bottom();
 				}else{
 					
 					console.log('stop');
@@ -38,38 +75,6 @@ function h_make_table_interval(){
 	}, 300);
 }
 
-$(document).ready(function(){
-
-	h_make_table_interval();
-	h_goto_bottom_of_screen();
-
-	if( $('#message') ){
-		$('#message').keypress(function(e) {
-			if(e.which == 13) {
-				$.ajax({
-					url: 'hash.php',
-					method: 'get',
-					data: {
-						action: 'post_message',
-						message: $('#message').val(),
-						nick: php_.nick,
-						hash: php_._hash
-					},
-					success: function(
-						data, 
-						textStatus, 
-						jqXHR
-					){
-						$('#message').val('');
-						$('#messages').html(data);
-						h_goto_bottom_of_screen();
-					}
-				});
-			}
-		});
-	}
-});
-
-function h_goto_bottom_of_screen(){
+function goto_screen_bottom(){
 	$(window).scrollTop( $(document).height() );
 }
