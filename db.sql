73aed0a3b3228597d278243e2989dccffa3ecf82
Aubrey Portwood <aubreypwd@gmail.com>
2014-02-09 20:33:03 -0700

    Including some setup stuff

---
+++ db.sql
@@ -0,0 +1,43 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: local.wordpress.dev (MySQL 5.5.35-0ubuntu0.12.04.2)
+# Database: hash_dev
+# Generation Time: 2014-02-10 03:20:20 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table messages
+# ------------------------------------------------------------
+
+CREATE TABLE `messages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nick` text,
+  `message` text,
+  `time` text,
+  `hash` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Hash
+
+Hash is a simple chat system designed to keep chats persistent, simple, hacker-friendly, and identity-flexible. 
+
+A few things to consider when using Hash:
+
+* A hash, is a chat room
+* Anyone can join a hash, so naming your hash "myhash" might not be a good idea, something like "myHash4356c9"
+* Hash names and Nicks are case sensitive, so a hash called "myhash4356c9" is different from "myHash4356c9"
+* Nicks belong to no one
+* Chats are persistent and cannot be erased except by someone who controls the database
+
+Hash was created because I wanted something like IRC, persistent and easy like HipChat, open source, and based
+on PHP, MySQL, HTML, and Javascript. Oh and **dead** simple!
+
+# Installation
+
+Clone this repo down and rename `config.php.sample` to `config.php` and populate with your values. Import
+`db.sql` to your database and start hashing it out!
+
+# Development
+
+There is a VVV setup for developing Hash over at 
+
+# Changelog
+
+## 0.8
+
+- Basic concept that works and was developed in Chrome
