b8d773ccbe77a4b9c8224f833edc9f6f98d90be7
Aubrey Portwood <aubreypwd@gmail.com>
2014-02-14 09:57:47 -0700

    Removed extra tags that were breaking links to specific messages

--- hash-table.html.php
+++ hash-table.html.php
@@ -17,7 +17,7 @@ $messages = the_messages( the_hash() );
 					<?php echo the_message($message['message']); ?>
 				</td>
 				<td class="column message-time">
-					<a href="?time=&hash=hash&nick=nick#<?php echo $message['id']; ?>" title="<?php echo $config->timezone; ?>">
+					<a href="#<?php echo $message['id']; ?>" title="<?php echo $config->timezone; ?>">
 						<?php echo date("F j, Y, g:i a", $message['time']); ?>
 					</a>
 				</td>
